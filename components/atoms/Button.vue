00b023ee7bc3b0ab8dc792137fcdea30d2cc94c8
kureharyosuke <ohhayo.com@gmail.com>
2022-12-14 22:13:24 +0900

    add: button component added

---
+++ components/atoms/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button :type="type" :disabled="disabled">
+    {{ value }}
+  </button>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  value: string;
+  type: "button" | "reset" | "submit";
+  disabled: boolean;
+  onClick: Function | undefined;
+};
+defineProps<Props>();
+</script>
+
+<style scoped></style>
