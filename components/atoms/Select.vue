3c7bbfb95bc275eb767a052d3d5a11bacb50976d
kureharyosuke <ohhayo.com@gmail.com>
2022-12-15 19:31:24 +0900

    add: select components

---
+++ components/atoms/Select.vue
@@ -0,0 +1,18 @@
+<template>
+  <select :id="id" :name="name">
+    <option v-for="{ id, value } in select" :key="id" :value="value">
+      {{ value }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  id: string;
+  name: string;
+  select: { id: number | string; value: string }[];
+};
+defineProps<Props>();
+</script>
+
+<style scoped></style>
