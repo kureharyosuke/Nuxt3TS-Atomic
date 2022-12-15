1f827bafe38cba128bcc3602745e613f7fa1e642
kureharyosuke <ohhayo.com@gmail.com>
2022-12-15 19:36:14 +0900

    add: List component added

---
+++ components/atoms/List.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li v-for="{ id, value } in list" :key="id">{{ value }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  list: { id: number | string; value: string }[];
+};
+defineProps<Props>();
+</script>
+
+<style scoped></style>
